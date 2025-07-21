36d851cf924a33486ac6a25d89214d106a835c30
Uranioa <uranioa@foxmail.com>
2025-07-22 00:13:33 +0800

    style: 整理项目代码格式
    - Format.py 代码格式整理
    - README.md 文件格式整理

--- module/Format.py
+++ module/Format.py
@@ -1,5 +1,5 @@
-def path_formatting(file_path):
-    if file_path.startswith('"') and file_path.endswith('"'):
-        return file_path[1:-1]
-    else:
-        return file_path
+def path_formatting ( file_path ):
+	if file_path.startswith ( '"' ) and file_path.endswith ( '"' ):
+		return file_path [ 1:-1 ]
+	else:
+		return file_path
