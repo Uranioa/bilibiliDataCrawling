9c7c131cc906abceecffac6429eb8da21ff9c231
Uranioa <uranioa@foxmail.com>
2025-07-23 00:45:50 +0800

    feat: 添加主程序入口逻辑
    - 添加主程序入口逻辑
    - 增强用户交互体验，添加输入提示和退出等待功能

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from module.ExcelProcessing import *
 from module.Spiders import *
 
-fileAddress = input ( "请输入文件地址：" )
-
-if is_excel_file ( fileAddress ):
-	video_url = read_excel ( fileAddress )
-	data_crawling ( fileAddress, video_url )
-else:
-	print ( "文件不是有效的 Excel 文件或文件不存在，请检查文件地址。" )
+if __name__ == '__main__':
+	fileAddress = input ( "请输入文件地址：" )
+	if is_excel_file ( fileAddress ):
+		video_url = read_excel ( fileAddress )
+		data_crawling ( fileAddress, video_url )
+	else:
+		print ( "文件不是有效的 Excel 文件或文件不存在，请检查文件地址。" )
+		input ( "按任意键退出。" )
